35fbd8aeb685736e059a1c6590e1435294570f46
Fabrício Souza <fabriciossza@gmail.com>
2025-11-11 14:41:39 -0300

    fix: adjust backend s3 key name

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "garagemanagement-terraform-backend" 
-    key            = "garage-management-app/terraform.tfstate"
+    key            = "garage-management-infra/terraform.tfstate"
     region         = "us-east-1"
 
     dynamodb_table = "garagemanagement-terraform-locks"
