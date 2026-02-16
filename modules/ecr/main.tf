5aeb2a047309c37c12d23d297a8e2f3e5303117c
Fabrício Souza <94847330+fabriciosouzza@users.noreply.github.com>
2026-02-16 17:34:30 -0300

    refactor: differentiate ECR module into a main repository and microservice repositories.

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,5 +1,17 @@
-resource "aws_ecr_repository" "repos" {
-  for_each = toset(var.repository_names)
+resource "aws_ecr_repository" "main" {
+  name = var.repository_name
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = var.repository_name
+  }
+}
+
+resource "aws_ecr_repository" "microservices" {
+  for_each = toset(var.microservice_names)
 
   name = each.value
 
--- modules/ecr/variables.tf
+++ modules/ecr/variables.tf
@@ -1,4 +1,10 @@
-variable "repository_names" {
-  description = "Lista de nomes dos repositórios ECR"
+variable "repository_name" {
+  description = "Nome do repositório ECR principal"
+  type        = string
+}
+
+variable "microservice_names" {
+  description = "Nomes dos repositórios ECR dos microserviços"
   type        = list(string)
+  default     = []
 }
